16b16067545eec3aed9104e50c238da12a473863
David Jones <david.jones02@sap.com>
2015-12-03 20:09:25 +0000

    fizzing and buzzing

---
+++ solutions/fizzBuzz.js
@@ -0,0 +1,27 @@
+'use strict';
+
+/**
+ * @desc Standard fizz buzz function
+ * @param {number} limit The number fizzbuzz needs to be played until
+ * @returns {Array} outputArray The array of outputs decided by fizzbuzz
+ */
+var fizzBuzz = function(limit) {
+
+	var outputArray = [];
+	
+	for(var i = 0; i < limit; i++) {
+		if(i % 3 === 0 && i % 5 === 0) {
+			outputArray[i] = 'fizzbuzz';
+		} else if(i % 3 === 0) {
+			outputArray[i] = 'fizz';
+		} else if(i % 5 === 0) {
+			 outputArray[i] = 'buzz';
+		} else {
+			outputArray[i] = i;
+		}
+	}
+	
+	return outputArray;
+};
+
+module.exports = fizzBuzz;
---
+++ specs/fizzbuzz.spec.js
@@ -0,0 +1,31 @@
+'use strict';
+
+var fizzBuzz = require('../solutions/fizzBuzz.js');
+
+describe('When using fizzbuzz()', function() {
+
+	it('should output the number if not divisible by 3 or 5', function() {
+		expect(fizzBuzz(100)[4]).toEqual(4);
+		expect(fizzBuzz(100)[28]).toEqual(28);
+		expect(fizzBuzz(100)[43]).toEqual(43);
+	});
+	
+	it('should output fizz when a number is divisible by 3', function() {
+		expect(fizzBuzz(100)[3]).toEqual('fizz');
+		expect(fizzBuzz(100)[69]).toEqual('fizz');
+		expect(fizzBuzz(100)[81]).toEqual('fizz');
+	});
+	
+	it('should output buzz when a number is divisible by 5', function() {
+		expect(fizzBuzz(100)[20]).toEqual('buzz');
+		expect(fizzBuzz(100)[55]).toEqual('buzz');
+		expect(fizzBuzz(100)[85]).toEqual('buzz');
+	});
+	
+	it('should output fizzbuzz when a number is divisible by both 3 and 5', function() {
+		expect(fizzBuzz(100)[15]).toEqual('fizzbuzz');
+		expect(fizzBuzz(100)[45]).toEqual('fizzbuzz');
+		expect(fizzBuzz(100)[90]).toEqual('fizzbuzz');
+	});
+
+});
